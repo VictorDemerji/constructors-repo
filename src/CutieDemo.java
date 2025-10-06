07d8def85b0a8d74e39cb063ea5011f22ed112dd
VictorDemerji <demerji.v1@gmail.com>
2025-10-06 16:27:16 +0300

    Add coments in English

--- src/CutieDemo.java
+++ src/CutieDemo.java
@@ -1,17 +1,16 @@
 public class CutieDemo {
     public static void main(String[] args) {
-        //declaram alocam si initializam obiecte ale clasei Cutie
 
         Cutie cutiaMea1 = new Cutie();
         Cutie cutiaMea2 = new Cutie();
 
         double volum;
 
-        //returnam volumul primei cutii
+        //return first box volume
         volum = cutiaMea1.volum();
         System.out.println("Volumu primei cutii este " + volum);
 
-        //volum la a doua cutie
+        //volume of the second box
         volum = cutiaMea2.volum();
         System.out.println("Volumul la a doua cutie este " + volum);
     }
